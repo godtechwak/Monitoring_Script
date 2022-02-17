21330f2ec6b3f6013f74349b57e9d7d203e87142
godtechwak <68718384+godtechwak@users.noreply.github.com>
2022-02-17 12:08:02 +0900

    Rename 3. Memory Check Job Create.sql to 2. Memory Check Job Create.sql

--- 1. Memory/2. Memory Check Job Create.sql
+++ 1. Memory/2. Memory Check Job Create.sql
@@ -3,7 +3,7 @@ GO
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 2021-11-24 ø¿»ƒ 2:22:32 ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 2021-11-24 ¬ø√Ä√à√Ñ 2:22:32 ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -21,9 +21,9 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'memory',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'[Uncategorized (Local)]', 
-		@owner_login_name=N'∑Œ±◊¿Œ∞Ë¡§', @job_id = @jobId OUTPUT
+		@owner_login_name=N'¬∑√é¬±√ó√Ä√é¬∞√®√Å¬§', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Memory Check]    Script Date: 2021-11-24 ø¿»ƒ 2:22:32 ******/
+/****** Object:  Step [Memory Check]    Script Date: 2021-11-24 ¬ø√Ä√à√Ñ 2:22:32 ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Memory Check', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
